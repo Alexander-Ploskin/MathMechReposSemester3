4bc4b50b1ebcf6ea47ab73340096c89eac61af19
Alexander-Ploskin <ploskin0107@gmail.com>
2020-09-14 17:20:53 +0300

    Removed path generator

--- 1.1/ConcurrentMatrixMultiplication/PathGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ConcurrentMatrixMultiplication
-{
-    public static class PathGenerator
-    {
-        public static string GetPathByName(string name) => Environment.CurrentDirectory.TrimEnd(@"\bin\Debug\netcoreapp3.1".ToCharArray()) + $@"ication\{name}";
-    }
-}
