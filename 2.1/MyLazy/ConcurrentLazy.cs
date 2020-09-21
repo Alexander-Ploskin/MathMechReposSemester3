f8bb39f3dcc4530baa7734ce391f039dbf03cfd7
Alexander-Ploskin <ploskin0107@gmail.com>
2020-09-21 12:17:31 +0300

    Made all

---
+++ 2.1/MyLazy/ConcurrentLazy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MyLazy
+{
+    /// <summary>
+    /// Implementation of ILazy for multithreading programs 
+    /// </summary>
+    /// <typeparam name="T">Type of a return value of the function</typeparam>
+    class ConcurrentLazy<T> : ILazy<T>
+    {
+        public ConcurrentLazy(Func<T> supplier)
+        {
+            if (supplier == null)
+            {
+                throw new ArgumentNullException();
+            }
+            this.supplier = supplier;
+        }
+
+        private readonly Object lockObject = new Object();
+
+        private readonly Func<T> supplier;
+
+        private T result = default(T);
+
+        private volatile bool calculated = false;
+
+        /// <summary>
+        /// Gives a result of the supplier function 
+        /// </summary>
+        /// <returns>Result of the calculation</returns>
+        public T Get()
+        {
+            if (calculated)
+            {
+                return result;
+            }
+
+            lock(lockObject)
+            {
+                if (calculated)
+                {
+                    return result;
+                }
+
+                result = supplier();
+                calculated = true;
+                return result;
+            }
+        }
+
+    }
+}
