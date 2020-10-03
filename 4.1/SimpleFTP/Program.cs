dbabf697bdba8d2b6fe9f1ddab7bdfaf86c23509
Alexander-Ploskin <ploskin0107@gmail.com>
2020-10-03 21:24:02 +0300

    started work

--- 4.1/SimpleFTP/Program.cs
+++ 4.1/SimpleFTP/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
 
 namespace SimpleFTP
 {
@@ -6,7 +9,22 @@ namespace SimpleFTP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int port = 8888;
+            var listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            Console.WriteLine($"Listening on port {port}...");
+            using (var socket = listener.AcceptSocket())
+            {
+                var stream = new NetworkStream(socket);
+                var reader = new StreamReader(stream);
+                var data = reader.ReadLine();
+                Console.WriteLine($"Received: {data}");
+                Console.WriteLine($"Sending \"Hi!\"");
+                var writer = new StreamWriter(stream);
+                writer.Write("Hi!");
+                writer.Flush();
+            }
+            listener.Stop();
         }
     }
 }
