85d433ef7ca5edd00745e62f1daf0434dd09cacf
Alexander-Ploskin <ploskin0107@gmail.com>
2020-10-20 14:47:32 +0300

    Restored files

---
+++ 4.1/FTPClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FTPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string host = "localhost";
+            const int port = 8888;
+
+        }
+    }
+}
