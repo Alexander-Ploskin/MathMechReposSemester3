f99e30e39ec95c40ae9b8d1ea5e657f6e0e16f1d
Alexander-Ploskin <ploskin0107@gmail.com>
2020-12-08 09:42:42 +0300

    Started

---
+++ ClassWorks/DirectoryMD5/DirectoryMD5/Checker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace DirectoryMD5
+{
+    public static class Checker
+    {
+        private static string CalculateCheckSum(DirectoryInfo info)
+        {
+            Console.WriteLine(info.GetFiles());
+            return info.GetFiles().ToString();
+        }
+
+        public static string CalculateChecksum(string path)
+        {
+            return CalculateCheckSum(new DirectoryInfo(path));
+        }
+    }
+}
---
+++ ClassWorks/DirectoryMD5/DirectoryMD5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DirectoryMD5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the path of directory to calculate checksum");
+            var path = Console.ReadLine();
+
+        }
+    }
+}
