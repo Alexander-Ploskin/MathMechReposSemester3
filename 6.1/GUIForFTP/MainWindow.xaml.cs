1d0f50ee3609147176efbc8b8f66f2dc8d22a0aa
Alexander-Ploskin <ploskin0107@gmail.com>
2021-01-08 17:13:37 +0300

    Made connection

--- 6.1/GUIForFTP/MainWindow.xaml.cs
+++ 6.1/GUIForFTP/MainWindow.xaml.cs
@@ -1,28 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace GUIForFTP
+namespace GUIforFTP
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Start the process, bind with model and list
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            var model = new ViewModel();
+            DataContext = model;
+
+            model.HaveMessage += ShowMessage;
+        }
+
+
+        private void ShowMessage(object sender, string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        private void ConnectClick(object sender, RoutedEventArgs e)
+        {
+            (DataContext as ViewModel).Connect();
         }
     }
 }
---
+++ 6.1/GUIForFTP/ViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace GUIforFTP
+{
+    public class ViewModel
+    {
+        private int port;
+        private FTPClient.FTPClient client;
+        private bool connected;
+
+        public event EventHandler<string> HaveMessage;
+
+        public ViewModel()
+        {
+            port = 49001;
+            ServerAddress = "localhost";
+            connected = false;
+        }
+
+        public string Port
+        {
+            get => port.ToString();
+            set
+            {
+                if (!int.TryParse(value, out port))
+                {
+                    HaveMessage?.Invoke(this, "Invalid value of port");
+                }
+            }
+        }
+
+        public string ServerAddress { get; set; }
+
+        public void Connect()
+        {
+            try
+            {
+                var tcpClient = new TcpClient(ServerAddress, port);
+                client = new FTPClient.FTPClient(tcpClient.GetStream());
+                connected = true;
+                HaveMessage?.Invoke(this, "Connected");
+            }
+            catch (SocketException)
+            {
+                HaveMessage?.Invoke(this, "Connection failed");
+            }
+        }
+    }
+}
